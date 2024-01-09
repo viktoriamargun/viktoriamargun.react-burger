f176568f76a33d3dc5c754d9e76e3323fc0581a3
viktoriamargun <154464080+viktoriamargun@users.noreply.github.com>
2024-01-09 13:55:30 +0300

    Обновление main

--- react-burger/src/components/main/burgeringredients/card-bun/card-bun.jsx
+++ react-burger/src/components/main/burgeringredients/card-bun/card-bun.jsx
@@ -15,22 +15,22 @@ CardBun.propTypes = {
 }
 
 export default function CardBun() {
-  const mainItems = data.map((item) => (item.type === 'bun' && (
-    <article key={item._id} className={styles.col_holder}>
+  const mainItems = data.map((props) => (props.type === 'bun' && (
+    <article key={props._id} className={styles.col_holder}>
       
       <Counter count={1} size="default" extraClass="m-1" />
       
       <div className={`${styles.col_img} ${'pl-4'} ${'pr-4'}`}>
-        <img src={item.image_large} alt={item.name} />
+        <img src={props.image_large} alt={props.name} />
       </div>
       
       <div className={`${styles.col_price} ${'pb-1'} ${'pt-1'}`}>
-        <p className={`${styles.col_price_p} ${'pr-2'} ${'text'} ${'text_type_digits-small'}`}>{item.price}</p>
+        <p className={`${styles.col_price_p} ${'pr-2'} ${'text'} ${'text_type_digits-small'}`}>{props.price}</p>
         <CurrencyIcon type="primary" />
       </div>
       
       <div className={styles.col_name}>
-        <p className={`${styles.col_name_p} ${'text'} ${'text_type_main-default'}`}>{item.name}</p>
+        <p className={`${styles.col_name_p} ${'text'} ${'text_type_main-default'}`}>{props.name}</p>
       </div>
     </article>
   )
